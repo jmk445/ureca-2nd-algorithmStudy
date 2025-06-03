84a8fdfc47602defc54e4c6c406393a925031c68
jmk445 <jmk445123456@gmail.com>
2025-06-03 19:49:58 +0900

    solved : p1051

--- jeongmin-kim/Algo/src/boj/p1051/P1051.java
+++ jeongmin-kim/Algo/src/boj/p1051/P1051.java
@@ -8,25 +8,31 @@ public class P1051 {
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(br.readLine());
-		int M = Integer.parseInt(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
 		int[][] map = new int[N][M];
+		
 		for(int i = 0; i < N; i ++) {
-			st = new StringTokenizer(br.readLine());
+			String line = br.readLine();			
 			for(int j = 0; j < M; j++) {
-				map[i][j] = Integer.parseInt(st.nextToken());
+				map[i][j] = (line.charAt(j))-'0';
 			}
 		}
 		int hor = N < M ? N : M;
 		
-		for(int i = 0; i < N; i++) {
-			for(int s = 0; s <= M - hor; s++) {
-				if(map[i][s + hor] == map[i][s] && map[i+hor][s] == map[i][s] && map[i+hor][s+hor] == map[i][s]) {
-					System.out.println(hor * hor);
-					return;
+			
+		while(hor!=0) {
+			for(int i = 0; i <= N - hor; i++) {
+				for(int s = 0; s <= M - hor; s++) {					
+					if(map[i][s + hor-1] == map[i][s] && map[i+hor-1][s] == map[i][s] && map[i+hor-1][s+hor-1] == map[i][s]) {
+						System.out.println(hor * hor);
+						return;
+					}
 				}
-			}
+			}			
+			hor--;
 		}
+		
 				
 		
 		
